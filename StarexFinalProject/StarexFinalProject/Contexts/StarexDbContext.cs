6274caef9a266298f330016cf188705fa0c5e23c
elsenmustafa <elshanjm@code.edu.az>
2020-11-06 00:01:30 +0400

    add

---
+++ StarexFinalProject/StarexFinalProject/Contexts/StarexDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StarexFinal.Data;
+using StarexFinal.Models;
+using StarexFinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarexFinalProject.Contexts
+{
+    public class StarexDbContext : IdentityDbContext<AppUsers>
+    {
+        public StarexDbContext(DbContextOptions<StarexDbContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<AppUsers> appUsers { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<UserBalance> UserBalances { get; set; }
+        public DbSet<Countries> Countries { get; set; }
+        public DbSet<Declarations> Declarations { get; set; }
+        public DbSet<DeclarationsStatus> DeclarationsStatuses { get; set; }
+        public DbSet<OnlineFormCategory> OnlineFormCategories { get; set; }
+        public DbSet<Warehouse> Warehouses { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+
+
+    }
+}
---
+++ StarexFinalProject/StarexFinalProject/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StarexFinal.Models;
+using StarexFinal.ViewModels;
+using StarexFinalProject.ViewModel;
+
+namespace StarexFinalProject.Controllers
+{
+    public class AccountController : Controller
+
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+        [HttpPost]
+        public IActionResult Register(UserViewModel userViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+               
+            }
+            return View();
+        }
+    }
+}
